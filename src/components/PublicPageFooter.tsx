aa8565642e67c769c0ff9beee71837fc78fbfc7f
jalexw <jalexwhitman@gmail.com>
2025-07-08 14:40:15 -0300

    0.0.27 - use next/image component for footer logo image

--- src/components/PublicPageFooter.tsx
+++ src/components/PublicPageFooter.tsx
@@ -2,6 +2,8 @@
 
 import { cn, Wordmark } from "@schemavaults/ui";
 import { Github, Linkedin, Twitter } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import type { FC, ReactElement } from "react";
 
 function ResourcesSectionLink({
@@ -72,16 +74,20 @@ export function PublicPageFooter({
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
-            <a href="https://schemavaults.com/" className="flex items-center">
-              <img
+            <Link
+              href="https://schemavaults.com"
+              className="flex items-center flex-row flex-nowrap gap-2 md:gap-4"
+            >
+              <Image
                 src="https://schemavaults.com/media/android-chrome-512x512.png"
-                className="h-8 me-3"
                 alt="SchemaVaults Logo"
+                width={48}
+                height={48}
               />
               <span className="self-center text-2xl font-semibold whitespace-nowrap">
                 <Wordmark />
               </span>
-            </a>
+            </Link>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
             <div>
