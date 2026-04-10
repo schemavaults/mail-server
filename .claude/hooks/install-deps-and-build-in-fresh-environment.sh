61c7aa363803648d23988cc6f4b1631cabf62022
jalexw <jalexwhitman@gmail.com>
2026-04-10 08:51:17 -0300

    only install deps on SessionStart, building is unnecessary

--- .claude/hooks/install-deps-and-build-in-fresh-environment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ ! -d "node_modules" ]; then
-  echo "No node_modules/ folder appears to exist so we're going to install deps and build so types exist"
-  bun install
-  bun run build
-fi
